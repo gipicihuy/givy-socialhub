76439fa9dc39402935600c1274cf683c0ddf040a
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2025-10-23 20:26:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,28 @@
-// config.js - Konfigurasi Icon & Logo Doang
+// config.js - File Konfigurasi Pusat (SIMPLE - Hanya Logo & Icon)
 export const CONFIG = {
-  // Profile Utama (index.html)
-  profileIcon: "https://raw.githubusercontent.com/upload-file-lab/fileupload6/main/uploads/1760408834748.png",
-  profileFavicon: "https://raw.githubusercontent.com/upload-file-lab/fileupload6/main/uploads/1760408834748.png",
-  
-  // IQC Maker (iqc/index.html)
-  iqcIcon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjgNFtKPgqKyS_jC5oOPR5_J1YLi5h6-JdNO-JoTIyzQG-CvPcfLqyP6RRLJPhu6yzKhrF7-HkhJq3y8_1l6bWPmmZ54Xu8-X79BFzF20viHbSAuyo6hSqlL5_43EYAWZduuh-ml61ZFqZqMiqXu6FIuyG2AI94NQBq2PoEQu2za9a4iPN-HHjASCvSRS0/w400-h248/Logo%20Monkey%20D.%20Luffy%20(Logo%20One%20Piece).png",
-  iqcFavicon: "https://qu.ax/aqgyC.jpg",
-  
-  // NGL Spam (ngl/index.html)
-  nglIcon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjgNFtKPgqKyS_jC5oOPR5_J1YLi5h6-JdNO-JoTIyzQG-CvPcfLqyP6RRLJPhu6yzKhrF7-HkhJq3y8_1l6bWPmmZ54Xu8-X79BFzF20viHbSAuyo6hSqlL5_43EYAWZduuh-ml61ZFqZqMiqXu6FIuyG2AI94NQBq2PoEQu2za9a4iPN-HHjASCvSRS0/w400-h248/Logo%20Monkey%20D.%20Luffy%20(Logo%20One%20Piece).png",
-  nglFavicon: "https://qu.ax/aqgyC.jpg",
-  
-  // Fake Tweet (tweet/index.html)
-  tweetIcon: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjgNFtKPgqKyS_jC5oOPR5_J1YLi5h6-JdNO-JoTIyzQG-CvPcfLqyP6RRLJPhu6yzKhrF7-HkhJq3y8_1l6bWPmmZ54Xu8-X79BFzF20viHbSAuyo6hSqlL5_43EYAWZduuh-ml61ZFqZqMiqXu6FIuyG2AI94NQBq2PoEQu2za9a4iPN-HHjASCvSRS0/w400-h248/Logo%20Monkey%20D.%20Luffy%20(Logo%20One%20Piece).png",
-  tweetFavicon: "https://qu.ax/aqgyC.jpg"
+  // EDIT INI AJA UNTUK GANTI LOGO & ICON SEMUA HALAMAN
+  favicon: "https://raw.githubusercontent.com/upload-file-lab/fileupload6/main/uploads/1760408834748.png",
+  profileImage: "https://raw.githubusercontent.com/upload-file-lab/fileupload6/main/uploads/1760408834748.png"
 };
+
+// Helper untuk load favicon
+export function loadFavicon() {
+  const link = document.querySelector('link[rel="icon"]');
+  if (link) {
+    link.href = CONFIG.favicon;
+  } else {
+    const newLink = document.createElement('link');
+    newLink.rel = 'icon';
+    newLink.type = 'image/png';
+    newLink.href = CONFIG.favicon;
+    document.head.appendChild(newLink);
+  }
+}
+
+// Helper untuk load profile image
+export function loadProfileImage() {
+  const img = document.getElementById('profile-image');
+  if (img) {
+    img.src = CONFIG.profileImage;
+  }
+}
